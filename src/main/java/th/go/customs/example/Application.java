1945fc1a5f3e4de2610d0cba8839b7ad17ea5a92
Nopparat   Siriban <59223002+nook01294@users.noreply.github.com>
2022-01-11 13:02:51 +0700

    update

--- src/main/java/th/go/customs/example/Application.java
+++ src/main/java/th/go/customs/example/Application.java
@@ -2,7 +2,11 @@ package th.go.customs.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableAsync
+@EnableTransactionManagement
 @SpringBootApplication
 public class Application {
 	
