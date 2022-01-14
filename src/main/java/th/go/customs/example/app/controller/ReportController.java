fc88f8557ce086455cd3b48e4b466a0774892fdc
Nopparat   Siriban <59223002+nook01294@users.noreply.github.com>
2022-01-14 17:08:45 +0700

    update upload excel

--- src/main/java/th/go/customs/example/app/controller/ReportController.java
+++ src/main/java/th/go/customs/example/app/controller/ReportController.java
@@ -10,24 +10,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import th.go.customs.example.app.service.ReportService;
-
+import th.go.customs.example.framework.bean.ResponseData;
+import th.go.customs.example.framework.bean.ProjectConstant.RESPONSE_MESSAGE;
+import th.go.customs.example.framework.bean.ProjectConstant.RESPONSE_STATUS;
 
 @RestController
 @RequestMapping("api/report")
 public class ReportController {
-	
+
 	@Autowired
 	private ReportService reportService;
-	
+
 	@GetMapping("/exportPdf")
 	@ResponseBody
-	public void exportPdfMTDREFcl( HttpServletResponse response) throws Exception {
-		byte[] reportFile = reportService.exportReport();
-		String filename = "Backend_Example.pdf";
-		response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", filename));
-		response.setContentType("application/octet-stream");
-		FileCopyUtils.copy(reportFile, response.getOutputStream());
-		
+	public void exportPdfMTDREFcl(HttpServletResponse response) throws Exception {
+		ResponseData<String> responseData = new ResponseData<String>();
+		try {
+
+			byte[] reportFile = reportService.exportReport();
+			String filename = "Backend_Example.pdf";
+			response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", filename));
+			response.setContentType("application/octet-stream");
+			FileCopyUtils.copy(reportFile, response.getOutputStream());
+
+			responseData.setMessage(RESPONSE_MESSAGE.SUCCESS);
+			responseData.setStatus(RESPONSE_STATUS.SUCCESS);
+
+		} catch (Exception e) {
+			responseData.setMessage(RESPONSE_MESSAGE.SAVE.FAILED);
+			responseData.setStatus(RESPONSE_STATUS.FAILED);
+		}
+
 	}
 
 }
--- src/main/java/th/go/customs/example/app/service/MailService.java
+++ src/main/java/th/go/customs/example/app/service/MailService.java
@@ -7,6 +7,7 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -17,8 +18,7 @@ import th.go.customs.example.app.vo.req.FwUserReq;
 @Service
 public class MailService {
 
-
-	public void sendMail(FwUserReq req) {
+	public void sendMail(FwUserReq req) throws AddressException, MessagingException {
 
 		String host = "smtp.gmail.com";
 		final String user = "nook01298@gmail.com";// change accordingly
@@ -40,21 +40,18 @@ public class MailService {
 		});
 
 		// Compose the message
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(user));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			message.setSubject("javatpoint");
-			message.setText("This is simple program of sending email using JavaMail API");
 
-			// send the message
-			Transport.send(message);
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(user));
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		message.setSubject("javatpoint");
+		message.setText("This is simple program of sending email using JavaMail API");
 
-			System.out.println("message sent successfully...");
+		// send the message
+		Transport.send(message);
 
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
+		System.out.println("message sent successfully...");
+
 	}
 
 }
