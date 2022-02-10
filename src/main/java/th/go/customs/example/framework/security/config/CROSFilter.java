2aaf8a6e62cfcf4fdf726ca7db2458478cc15840
Nopparat   Siriban <59223002+nook01294@users.noreply.github.com>
2022-02-10 14:56:19 +0700

    update cros

---
+++ src/main/java/th/go/customs/example/framework/security/config/CROSFilter.java
@@ -0,0 +1,41 @@
+package th.go.customs.example.framework.security.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CROSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException { }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,OPTIONS,DELETE");
+        response.setHeader("Access-Control-Max-Age", String.valueOf(60000l * 60l));
+        response.setHeader("Access-Control-Allow-Headers", "Authorization,X-HTTP-Method-Override,Accept,Accept-Language,Content-Language,Content-Type");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+
+    }
+
+    @Override
+    public void destroy() { }
+
+}
+
